88a62f23b7af1e515b984a0429b4747008794c93
preda <preda@a0fca8a9-8b17-0410-a33d-9dbc2cbb4962>
2006-11-27 19:53:30 +0000

    handle soft keys for menu in full screen

--- src/Config.java
+++ src/Config.java
@@ -1,7 +1,15 @@
 import java.util.*;
 import java.io.*;
+import javax.microedition.lcdui.*;
 
-class Config {
+/*
+  The fact that Config extends Canvas is the result of a 'class merge',
+  in order to reduce the number of classes. 
+  Otherwise these two, the config and the canvas, have nothing to do with each other.
+  The canvas is used exculsively for getActionCode(), and is never displayed.
+*/
+
+class Config extends Canvas {
     Hashtable ht = new Hashtable();
     RMS rs;
     int recId;
@@ -58,5 +66,10 @@ class Config {
         }
         rs.write(recId);
         System.out.println("config saved");
+    }
+
+    //---- Canvas below, not related to Config above ----
+    
+    protected void paint(Graphics g) {
     }
 }
