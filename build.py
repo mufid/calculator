156aa21bea25f311be9197415b2de71e9badf494
preda <preda@a0fca8a9-8b17-0410-a33d-9dbc2cbb4962>
2007-07-24 23:54:56 +0000

    python build, external from menstral

---
+++ build.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import mebuild
+import glob
+import shutil
+from os import path
+
+spec = dict(
+cldc    = '1.1',
+midp    = '2.0',
+name    = 'Calculator',
+icon    = 'a',
+cls     = 'C',
+version = '0.1.3',
+author  = 'Mihai Preda & Carlo Teubner',
+infoUrl = 'http://calculator.javia.org/',
+extralibs = []
+)
+
+midlet = mebuild.Midlet(**spec)
+
+midlet.process(midlet.fileName, midlet.tmpDir)
