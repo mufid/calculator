18da5de2a35c013409064e43ef45da0d3c400a35
carlo <carlo@a0fca8a9-8b17-0410-a33d-9dbc2cbb4962>
2007-08-17 16:19:50 +0000

    - drawing of axes (plot, par) and labels (plot, par, map) is now optional

--- src/CalcConfig.java
+++ src/CalcConfig.java
@@ -6,12 +6,18 @@ class CalcConfig {
         kAngleKey = "angleUnit",
         kAngleRad = "rad",
         kAngleDeg = "deg",
-        kRoundKey = "roundDigits";
+        kRoundKey = "roundDigits",
+        kAxes     = "axes",
+        kLabels   = "labels",
+        kTrue     = "T",
+        kFalse    = "F";
        
     CalcConfig(Store rs, int recId) {
         cfg = new Config(rs, recId);
         angleInRadians = cfg.get(kAngleKey, kAngleRad).equals(kAngleRad);
         roundingDigits = Integer.parseInt(cfg.get(kRoundKey, "1"));
+        axes = cfg.get(kAxes, kTrue).equals(kTrue);
+        labels = cfg.get(kLabels, kTrue).equals(kTrue);
     }
 
     void setAngleInRadians(boolean inRad) {
@@ -26,8 +32,22 @@ class CalcConfig {
         cfg.save();
     }
 
+    void setAxes(boolean axes) {
+        this.axes = axes;
+        cfg.set(kAxes, axes ? kTrue : kFalse);
+        cfg.save();
+    }
+
+    void setLabels(boolean labels) {
+        this.labels = labels;
+        cfg.set(kLabels, labels ? kTrue : kFalse);
+        cfg.save();
+    }
+
     boolean angleInRadians;
     int roundingDigits;
+    boolean axes;
+    boolean labels;
 
     private Config cfg;
 }
