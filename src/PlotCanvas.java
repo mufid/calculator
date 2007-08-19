db000d824bb2a13e7243d2125611d70c0804bdff
preda <preda@a0fca8a9-8b17-0410-a33d-9dbc2cbb4962>
2007-08-19 12:23:05 +0000

    PlotCanvas init; some COLOR consts; paintMap mem usage; curly brackets

--- src/PlotCanvas.java
+++ src/PlotCanvas.java
@@ -9,10 +9,16 @@ import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
 
 public class PlotCanvas extends Canvas implements VMConstants {
-
     private final static int PARPLOT_INIT_POINTS = 64; // initial number of points - must be power of 2
     private final static int PARPLOT_MAX_POINTS = 4096; // max number of points - must be power of 2
     private final static int PARPLOT_MAX_DIST = 15; // max squared distance in pixels between neighbouring points
+
+    private final static int 
+        COLOR_BLACK = 0x000000,
+        COLOR_WHITE = 0xffffff,
+        COLOR_RED   = 0xff0000,
+        COLOR_DARK_BLUE = 0x000080,
+        COLOR_LABEL = 0x7070ff;
 
     private CompiledFunction func, func2;
     private double[] minmax;
@@ -24,29 +30,25 @@ public class PlotCanvas extends Canvas implements VMConstants {
     private Display display;
     private Displayable next;
     
-    public PlotCanvas(Display display) {
+    public PlotCanvas(Display display, Displayable next) {
         this.display = display;
-        setFullScreenMode(true);
-    }
+        this.next = next;
 
-    public void init(Displayable next) {
-        this.next = next;        
+        setFullScreenMode(true);
+        width  = getWidth();
+        height = getHeight();
+        smallFont = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_SMALL);
     }
 
     public void plot(Result result) {
         func = result.function;
         func2 = result.function2;
         minmax = result.plotArgs;
-        width = getWidth();
-        height = getHeight();
-        if (smallFont == null)
-            smallFont = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_SMALL);
-
         img = Image.createImage(width, height);
         Graphics g = img.getGraphics();
         g.setFont(smallFont);
-        switch (result.plotCommand)
-        {
+
+        switch (result.plotCommand) {
         case PLOT: paintPlot(g); break;
         case MAP: paintMap(g); break;
         case PARPLOT: paintParPlot(g); break;
@@ -59,6 +61,10 @@ public class PlotCanvas extends Canvas implements VMConstants {
         g.drawImage(img, 0, 0, Graphics.TOP | Graphics.LEFT);
     }
 
+    private static String doubleToString(double v, double maxError) {
+        return Util.doubleToString(Util.shortApprox(v, maxError), 1);
+    }
+
     private void paintPlot(Graphics g) {
         final double xmin = minmax[0], xmax = minmax[1];
         final double xf = (xmax - xmin) / (width - 1);
@@ -66,21 +72,27 @@ public class PlotCanvas extends Canvas implements VMConstants {
         double ymax = Double.NEGATIVE_INFINITY;
         double[] y = new double[width];
 
-        for (int xp = 0; xp < width; ++xp) {
-            double v = func.evaluate(xmin + xp * xf);
+        double x = xmin;
+        for (int xp = 0; xp < width; ++xp, x += xf) {
+            double v = func.evaluate(x);
             if (isReal(v)) {
-                if (v < ymin)
+                if (v < ymin) {
                     ymin = v;
-                if (v > ymax)
+                }
+                if (v > ymax) {
                     ymax = v;
+                }
             }
             y[xp] = v;
         }
     
         final double yf = (ymax - ymin) / (height - 1);
 
-        g.setColor(0x00FFFFFF);
-        g.fillRect(0, 0, width, height);
+        // The newly-created image is all-white (according to MIDP spec),
+        // so there's no need to white-fill it here.
+        // We'd need to clear it if we'd reuse the image between plots.
+        // g.setColor(COLOR_WHITE);
+        // g.fillRect(0, 0, width, height);
 
         if (C.cfg.axes) {
             g.setGrayScale(180);
@@ -95,103 +107,90 @@ public class PlotCanvas extends Canvas implements VMConstants {
         }
 
         if (C.cfg.labels) {
-            g.setColor(0x007070FF);
-            // XXX Mihai: format(ymin, xf)?
-            g.drawString(Double.toString(ymin), 0, height-1, Graphics.BOTTOM | Graphics.LEFT);
-            g.drawString(Double.toString(ymax), 0, 0, Graphics.TOP | Graphics.LEFT);
+            g.setColor(COLOR_LABEL);
+            g.drawString(doubleToString(ymin, xf), 0, height-1, Graphics.BOTTOM | Graphics.LEFT);
+            g.drawString(doubleToString(ymax, xf), 0, 0, Graphics.TOP | Graphics.LEFT);
         }
 
-        g.setColor(0x00000000);
-        if (ymin == ymax)
+        g.setColor(COLOR_BLACK);
+        if (ymin == ymax) {
             g.drawLine(0, height/2, width-1, height/2);
-        else {
+        } else {
             int y1 = (int) ((y[0] - ymin) / yf + 0.5);
             for (int xp = 1; xp < width; ++xp) {
                 int y2 = (int) ((y[xp] - ymin) / yf + 0.5);
-                if (isReal(y[xp-1]) && isReal(y[xp]))
+                if (isReal(y[xp-1]) && isReal(y[xp])) {
                     g.drawLine(xp - 1, height - 1 - y1, xp, height - 1 - y2);
+                }
                 y1 = y2;
             }
         }
     }
 
     private void paintMap(Graphics g) {
-        long start, end;
-
         int canvasHeight, fontHeight = 0, infoHeight = 0;
         
         if (C.cfg.labels) {
             fontHeight = smallFont.getHeight();
             infoHeight = fontHeight + 2;
             canvasHeight = height - infoHeight;
-        } else
+        } else {
             canvasHeight = height;
+        }
 
         final double xmin = minmax[0], xmax = minmax[1], ymin = minmax[2], ymax = minmax[3];
         final double xf = (xmax - xmin) / (width - 1);
         final double yf = (ymax - ymin) / (canvasHeight - 1);
-        double fmin = Double.POSITIVE_INFINITY, fmax = Double.NEGATIVE_INFINITY;
+        float fmin = Float.POSITIVE_INFINITY, fmax = Float.NEGATIVE_INFINITY;
 
         final int size = width*canvasHeight;
-        double[] f = new double[size];
-        start = System.currentTimeMillis();
-        for (int x = 0; x < width; ++x)
-            for (int y = 0; y < canvasHeight; ++y) {
-                double v = func.evaluate(xmin + x * xf, ymin + y * yf);
-                if (isReal(v)) {
-                    if (v < fmin)
-                        fmin = v;
-                    if (v > fmax)
-                        fmax = v;
+
+        // We store the function values as floats in an int array.
+        // The float precision is enough for plot/map.
+        // We use int array in order to reuse it for createRGBImage() later.
+        int[] f = new int[size];
+        long start = System.currentTimeMillis();
+        {   // This block (curly brackets) is used to limit the scope of xval.
+            double xval = xmin;
+            for (int x = 0; x < width; ++x, xval += xf) {
+                double yval = ymin;
+                for (int y = 0; y < canvasHeight; ++y, yval += yf) {
+                    float v = (float) func.evaluate(xval, yval);
+                    if (isReal(v)) {
+                        if (v < fmin) {
+                            fmin = v;
+                        } else if (v > fmax) {
+                            fmax = v;
+                        }
+                    }
+                    f[x + (canvasHeight - 1 - y)*width] = Float.floatToIntBits(v);
                 }
-                f[x + (canvasHeight - 1 - y)*width] = v;
             }
-        end = System.currentTimeMillis();
+        }
+
+        long end = System.currentTimeMillis();
         Log.log("Calculation took " + (end - start) + " ms.");
 
-        /* The following straightforward method of going from the array of function values (f)
-           to the array of colour values (rgb) is quite memory-intensive. This cost could be
-           reduced by doing the following instead:
-
-           int[] f1 = new int[size];
-           double[] f2 = new int[size/2];
-
-           put the first half of the function values into f1, using MoreMath.HI and MoreMath.LO;
-           put the second half of the function values into f2;
-           convert f1 and f2 to colour values, which we store in f1(!);
-           f2 = null;
-           ... Image.createRGBImage(f1, ...);
-           f1 = null;
-           
-           If the speed penalty for the above is significant, it could be a fallback in case
-           of an OutOfMemoryError.
-           
-           A more extreme memory-saving, speed-decreasing measure would be to evaluate our function
-           twice at each pixel, once purely to determine fmin, fmax, and once to populate rgb.
-         */
-
+        start = end;
         final double gf = 255 / (fmax - fmin);
-        start = System.currentTimeMillis();
-        int[] rgb = new int[size];
-        double v;
+        float v;
         int c;
         for (int i = 0; i < size; ++i) {
-            v = f[i];
-            if (Double.isNaN(v))
-                c = 0xFF0000;
-            else if (Double.NEGATIVE_INFINITY == v)
-                c = 0x000080;
-            else if (Double.POSITIVE_INFINITY == v)
+            v = Float.intBitsToFloat(f[i]);
+            if (Float.isNaN(v)) {
+                c = COLOR_RED;
+            } else if (Float.NEGATIVE_INFINITY == v) {
+                c = COLOR_DARK_BLUE;
+            } else if (Float.POSITIVE_INFINITY == v) {
                 c = 0x40FFFF;
-            else {
+            } else {
                 c = (int) ((v - fmin) * gf + 0.5);
                 c |= (c << 8) | (c << 16); 
             }
-            rgb[i] = c;
+            f[i] = c;
         }
+        Image im = Image.createRGBImage(f, width, canvasHeight, false);
         f = null;
-        Image im = Image.createRGBImage(rgb, width, canvasHeight, false);
-        rgb = null;
         g.drawImage(im, 0, 0, Graphics.TOP | Graphics.LEFT);
         im = null;
         
@@ -358,5 +357,4 @@ public class PlotCanvas extends Canvas implements VMConstants {
         img = null;
         display.setCurrent(next);
     }
-
 }
