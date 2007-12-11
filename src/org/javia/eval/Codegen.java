eb204a4a6f75d682d4f6ae090a122c83b9adb77d
preda <preda@a0fca8a9-8b17-0410-a33d-9dbc2cbb4962>
2007-12-11 06:37:23 +0000

    pipeline Compiler to Codegen

--- src/org/javia/eval/Codegen.java
+++ src/org/javia/eval/Codegen.java
@@ -75,75 +75,75 @@ class Codegen {
         return null;
     }
 
-    Fun gen(Vector tokens) {
-        init();
-        int size = tokens.size();
+    boolean add(Token token) {
         double lastConst = 0;
         Fun lastFun = null;
-
-        for (int i = 0; i < size; ++i) {
-            byte op;
-
-            Token token = (Token) tokens.elementAt(i);
-            TokenType type = token.type;
-            switch (type.id) {
-            case Lexer.NUMBER:
-                op = Fun.CONST;
-                lastConst = consts[nConst++] = token.value;
-                break;
-
-            case Lexer.CONST:
-                String name = token.name;
-                if (name.equals("rnd")) {
-                    op  = Fun.RND;
-                } else {
-                    if (name.length() == 1) {
-                        char c = name.charAt(0);
-                        if (c == 'x' || c == 'y' || c == 'z') {
-                            op = (byte) (Fun.LDX + (c - 'x'));
-                            if (arity < c - 'x' + 1) {
-                                arity = c - 'x' + 1;
-                            }
-                            break;
-                        }
-                    }
-                    op = Fun.CONST;
-                    lastConst = consts[nConst++] = lookupVar(name);
-                }
-                break;
-
-            case Lexer.CALL:
-                op = lookupBuiltin(token.name, token.arity);
-                if (op <= 0) {
-                    op = Fun.CALL;
-                    lastFun = funcs[nf++] = lookupFun(token.name, token.arity);
-                }
-                break;
-
-            default:
-                op = type.vmop;
-                if (op <= 0) {
-                    throw new Error("wrong vmop");
-                }
-            }
-            int oldSP = sp;
-            sp = tracer.trace(stack, sp, op, lastConst, lastFun);
-            if (op == Fun.RND) {
-                stack[sp] = Double.NaN;
-            }
-            if (sp > oldSP || stack[sp] == Double.NaN) {
-                code[pc++] = op;
+        byte op;
+        TokenType type = token.type;
+        switch (type.id) {
+        case Lexer.NUMBER:
+            op = Fun.CONST;
+            lastConst = consts[nConst++] = token.value;
+            break;
+            
+        case Lexer.CONST:
+            String name = token.name;
+            if (name.equals("rnd")) {
+                op  = Fun.RND;
             } else {
-                //constant folding
-                pc -= oldSP - sp;
-                nConst -= oldSP - sp;
-                consts[nConst-1] = stack[sp];
-                if (code[pc-1] != Fun.CONST) {
-                    throw new Error("Expected CONST on fold");
+                if (name.length() == 1) {
+                    char c = name.charAt(0);
+                    if (c == 'x' || c == 'y' || c == 'z') {
+                        op = (byte) (Fun.LDX + (c - 'x'));
+                        if (arity < c - 'x' + 1) {
+                            arity = c - 'x' + 1;
+                        }
+                        break;
+                    }
                 }
+                op = Fun.CONST;
+                lastConst = consts[nConst++] = lookupVar(name);
+            }
+            break;
+            
+        case Lexer.CALL:
+            op = lookupBuiltin(token.name, token.arity);
+            if (op <= 0) {
+                op = Fun.CALL;
+                lastFun = funcs[nf++] = lookupFun(token.name, token.arity);
+            }
+            break;
+            
+        default:
+            op = type.vmop;
+            if (op <= 0) {
+                throw new Error("wrong vmop");
             }
         }
-        
+        int oldSP = sp;
+        sp = tracer.trace(stack, sp, op, lastConst, lastFun);
+        if (op == Fun.RND) {
+            stack[sp] = Double.NaN;
+        }
+        if (sp > oldSP || stack[sp] == Double.NaN) {
+            code[pc++] = op;
+        } else {
+            //constant folding
+            pc -= oldSP - sp;
+            nConst -= oldSP - sp;
+            consts[nConst-1] = stack[sp];
+            if (code[pc-1] != Fun.CONST) {
+                throw new Error("Expected CONST on fold");
+            }
+        }
+        return true;
+    }
+    
+    Fun getFun() {
+        if (pc <= 0) {
+            throw new Error("empty fun");
+        }
+
         double[] trimmedConsts = new double[nConst];
         System.arraycopy(consts, 0, trimmedConsts, 0, nConst);
 
@@ -154,8 +154,15 @@ class Codegen {
         byte[] trimmedCode = new byte[pc];
         System.arraycopy(code, 0, trimmedCode, 0, pc);
 
-        return pc > 1 ? 
-            new Fun(arity, trimmedCode, trimmedConsts, trimmedFuncs) :
-            null;
+        init();
+        return new Fun(arity, trimmedCode, trimmedConsts, trimmedFuncs);
+    }
+
+    Fun gen(Vector tokens) {
+        int size = tokens.size();
+        for (int i = 0; i < size; ++i) {
+            add((Token) tokens.elementAt(i));
+        }
+        return getFun();
     }
 }
